e179e1547dac19b9857a360bc37b1775c03368de
Gabriel Gomes Da silva <gomesgabriel0006@gmail.com>
2025-09-25 09:57:09 -0300

    Add files via upload

---
+++ src/service-worker.js
@@ -0,0 +1,19 @@
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open("app-saudacao-cache").then(cache => {
+      return cache.addAll([
+        "index.html",
+        "manifest.json",
+        "icone-192.png",
+        "icone-512.png",
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
